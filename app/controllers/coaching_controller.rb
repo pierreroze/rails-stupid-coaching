e2715bdfd83dddd22f8f7d55b75ff11d677dcb77
pierre roze <pierre.roze@ieseg.fr>
2017-02-13 14:22:44 -0200

    Ex. 1

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,14 +1,23 @@
-class CoachingController
-
-  def Answer
-
-  end
-
-  def Ask
+class CoachingController < ApplicationController
+  def ask
 
   end
 
-
+  def answer
+    @question = params[:query]
+  end
 
 end
 
+
+
+  #if your_message == "I am going to work right now!"
+    ""
+
+  #elsif your_message .include?"?"
+  #{}"Silly question, get dressed and go to work!"
+
+#  else
+ # "I don't care, get dressed and go to work!"
+ #end
+#end
